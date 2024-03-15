df38ff2335129995c4086d8d36e63228965e629b
anhngoc <tabaoanhngoc@gmail.com>
2024-03-15 18:44:57 +0700

    style: style game mode card

--- src/components/game-mode-card/game-mode-card.jsx
+++ src/components/game-mode-card/game-mode-card.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import './game-mode-card.css'
+import styles from './game-mode-card.module.css'
 
 function GameModeCard(props) {
   return (
-    <div>        
-        <div className="card-box">
-            <div className="level-number">{props.level}x{props.level}</div>
-            <div className="level-text">{props.difficulty}</div>
-        </div>
+
+    <div className={styles['card-box']}>
+      <div className={styles['level-number']} >{props.level}x{props.level}</div>
+      <div className={styles['level-text']}>{props.difficulty}</div>
     </div>
+
   )
 }
 
--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -15,7 +15,7 @@ function HomePage() {
       <div className={styles.banner}>
         <div className="main-content">
         <div>
-        <a href="https://www.pokemon.com/" target="_blank">
+        <a href="https://merctrans.vn/" target="_blank">
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png' className="logo" alt="Pokemon logo" />
         </a>
 
@@ -27,7 +27,7 @@ function HomePage() {
         <div className={styles['select-mode']}>
             <strong>Select Mode</strong>
         </div>
-        <div className="game-mode-card">
+        <div className={styles['game-mode-card']}>
           {gameMode.map((mode) => (
             <GameModeCard
               level={mode.level}
