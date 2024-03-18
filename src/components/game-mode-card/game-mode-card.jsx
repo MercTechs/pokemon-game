7805a269dd69002a86dfe212cb47c4b952a4235a
Sharko0511 <duyquang511@gmail.com>
2024-03-18 10:40:05 +0700

    feat: :sparkles: Add game screen

--- src/components/game-mode-card/game-mode-card.jsx
+++ src/components/game-mode-card/game-mode-card.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import './game-mode-card.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./game-mode-card.css";
 
 function GameModeCard(props) {
+    const navigate = useNavigate();
+
+    function handleClick() {
+      navigate(`/game/${props.level}`);
+    }
   return (
-    <div>        
-        <div className="card-box">
-            <div className="level-number">{props.level}x{props.level}</div>
-            <div className="level-text">{props.difficulty}</div>
+    <div onClick={handleClick}>
+      <div className="card-box">
+        <div className="level-number">
+          {props.level}x{props.level}
         </div>
+        <div className="level-text">{props.difficulty}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GameModeCard
+export default GameModeCard;
