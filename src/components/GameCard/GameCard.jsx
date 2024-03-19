ff46a80bdd3f9c6ee81a449724b9cc7019a652ed
anhngoc <tabaoanhngoc@gmail.com>
2024-03-19 16:10:47 +0700

    style: :lipstick: add ;

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,36 +1,53 @@
 import PropTypes from "prop-types";
-import styles from './GameCard.module.css'
+import styles from "./GameCard.module.css";
 
 function GameCard(props) {
-  let lastClick
+  let lastClick;
 
   function handleClickCard(e) {
-    lastClick = props.imgId
-    e.target.classList.toggle('click')
-    e.target.style.zIndex = 0
-    console.log('hi', e.target)
+    lastClick = props.imgId;
+    e.target.classList.toggle("click");
+    e.target.style.zIndex = 0;
+    console.log("hi", e.target);
+
+    const flipCard = document.querySelector(".flip-card-inner ");
+    flipCard.classList.toggle("flip");
+    console.log(flipCard);
   }
 
   return (
-
-    <div className={styles["flip-card"]}  >
+    <div className={styles["flip-card"]} onClick={handleClickCard}>
       <div className={styles["flip-card-inner"]}>
         <div className={styles["flip-card-front"]}>
-          <img style={{ maxWidth: "70px" , position: 'absolute', top: '20%', left: '9%' }}
+          <img
+            style={{
+              maxWidth: "70px",
+              position: "absolute",
+              top: "20%",
+              left: "9%",
+            }}
             alt="random"
             src={`/src/img/pokemons/back-card/PokeBall1.png`}
-            className={styles["card-front"]} />
+            className={styles["card-front"]}
+          />
         </div>
         <div className={styles["flip-card-back"]}>
-          <img style={{ maxWidth: "70px" , position: 'absolute', top: '20%', left: '9%' }}
+          <img
+            style={{
+              maxWidth: "70px",
+              position: "absolute",
+              top: "20%",
+              left: "9%",
+            }}
             src={`/src/img/pokemons/${props.imgId}.png`}
             alt="random"
-            className={styles["card-back"]} />
+            className={styles["card-back"]}
+          />
         </div>
         <div>{props.imgId}</div>
       </div>
     </div>
-  )
+  );
 }
 
 GameCard.propTypes = {
--- src/components/game-mode-card/game-mode-card.jsx
+++ src/components/game-mode-card/game-mode-card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import styles from './game-mode-card.module.css'
+import styles from "./game-mode-card.module.css";
 
 function GameModeCard(props) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleClick() {
-      navigate(`/game/${props.level}`);
-    }
+  function handleClick() {
+    navigate(`/game/${props.level}`);
+  }
   return (
     <div onClick={handleClick}>
-      <div className={styles['card-box']}>
-        <div className={styles['level-number']}>
+      <div className={styles["card-box"]}>
+        <div className={styles["level-number"]}>
           {props.level}x{props.level}
         </div>
-        <div className={styles['level-text']}>{props.difficulty}</div>
+        <div className={styles["level-text"]}>{props.difficulty}</div>
       </div>
     </div>
   );
