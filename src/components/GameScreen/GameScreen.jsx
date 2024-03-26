3efef39ab91e32d6d3e15e1f09132b17602db4e6
Sharko0511 <duyquang511@gmail.com>
2024-03-26 15:54:02 +0700

    fix: :ambulance: Remake game logic

--- src/components/GameScreen/GameScreen.jsx
+++ src/components/GameScreen/GameScreen.jsx
@@ -12,90 +12,45 @@ function GameScreen() {
   const navigate = useNavigate();
   const imagePairs = useMemo(() => generateImagePairs(level), [level]);
 
-  // State to track flipped cards
-  const [flippedCards, setFlippedCards] = useState([]);
-  const [cardBonuses, setCardBonuses] = useState([]); // Store bonuses for each card
-  const [totalPoints, setTotalPoints] = useState(0); // Total points state
-
-  // Function to handle flipping of cards
-  const handleCardFlip = (index, imgId) => {
-    if (flippedCards.some((card) => card.index === index)) {
-      return;
-    }
-    // If there are already 2 cards flipped and not yet handled, reset them
-    if (flippedCards.length === 2) {
-      // Reset immediately if a third card is clicked
-      setFlippedCards([{ index, imgId }]);
-      updateCardBonuses();
-    } else {
-      // Add the new flipped card to the state
-      setFlippedCards([...flippedCards, { index, imgId }]);
-    }
-  };
-
-  useEffect(() => {
-    // Initialize bonuses for each card to 60
-    setCardBonuses(new Array(imagePairs.length).fill(50));
-  }, [imagePairs.length]);
+  const [checkCards, setCheckCards] = useState([]);
 
   const handleBack = () => {
     navigate("/");
   };
 
-  // Function to update card bonuses
-  const updateCardBonuses = () => {
-    setCardBonuses((prevBonuses) =>
-      prevBonuses.map((bonus, index) =>
-        flippedCards.some((card) => card.index === index)
-          ? Math.max(bonus - 10, 0) // Ensure the bonus doesn't go below 0
-          : bonus
-      )
-    );
-  };
-
-  useEffect(() => {
-    // Log cardBonuses whenever it changes
-    console.log("cardBonuses:", cardBonuses);
-  }, [cardBonuses, updateCardBonuses]);
-
-  // State to keep track of matched cards
-  const [matchedCards, setMatchedCards] = useState([]);
-
-  // Effect to handle the flipping back logic after 1 second
-  useEffect(() => {
-    if (flippedCards.length === 2 && flippedCards.some((card) => !matchedCards.includes(card.index))) {
-      const [firstCard, secondCard] = flippedCards;
-      if (firstCard.imgId === secondCard.imgId) {
-        // If the image IDs match, leave the cards flipped and calculate points
-        const timer = setTimeout(() => {
-          console.log("match==============");
-          const bonusPoints = cardBonuses[firstCard.index] + cardBonuses[secondCard.index];
-          const totalPoints = 100 + bonusPoints;
-          // Update total points
-          setTotalPoints((prevPoints) => prevPoints + totalPoints);
-          // Set matched cards to be hidden
-          setMatchedCards((prevMatchedCards) => [...prevMatchedCards, firstCard.index, secondCard.index]);
-        }, 1000);
-        return () => clearTimeout(timer);
-      } else {
-        // If the image IDs don't match, flip the cards back after 1 second
-        const timer = setTimeout(() => {
-          setFlippedCards([]);
-          updateCardBonuses(); // Update card bonuses after flipping back
-        }, 1000);
-        return () => clearTimeout(timer);
+  const totalPoints = useMemo(() => {
+    let points = 0;
+    for (const card of imagePairs) {
+      if (card.isComplete) {
+        points = points + (card.basePoints + card.bonusPoints);
       }
     }
-  }, [flippedCards, updateCardBonuses, cardBonuses, matchedCards]);
+    return points;
+  }, [checkCards]);
+
+  useEffect(() => {
+    if (checkCards.length === 2) {
+      const [firstCard, secondCard] = checkCards;
+      if (firstCard.id === secondCard.id) {
+        firstCard.isComplete = true;
+        secondCard.isComplete = true;
+      } else {
+        for (const card of checkCards) {
+          card.isFlip = false;
+          card.bonusPoints = card.bonusPoints === 0 ? 0 : card.bonusPoints - 10;
+        }
+      }
+      setCheckCards([]);
+      console.log("pairs===============", imagePairs);
+    }
+  }, [checkCards]);
 
   return (
     <div className={styles["game-screen"]}>
       <div className={styles["back-btn"]}>
         <button onClick={handleBack}>Back to Main</button>
       </div>
-      <div>
-        <p>Total Points: {totalPoints}</p> {/* Display total points */}
-      </div>
+      <div className={styles["total-points"]}>{totalPoints}</div>
       <div
         className={styles["game-board"]}
         style={{
@@ -103,18 +58,23 @@ function GameScreen() {
           gridTemplateColumns: `repeat(${gridSize}, 1fr)`,
         }}
       >
-        {imagePairs.map((id, index) => (
-          <div key={index} className={`${styles["game-cell"]} ${matchedCards.includes(index) ? styles["hidden"] : ''}`}>
+        {imagePairs.map((card, index) => (
+          <div key={index} className={`${styles["game-cell"]}}`}>
             {/* Pass the handleCardFlip function, whether the card is flipped, and the card's bonus to GameCard */}
+
             <GameCard
               key={index}
               id={index}
-              imgId={id}
-              onCardClick={() => handleCardFlip(index, id)}
-              isFlipped={flippedCards.some((card) => card.index === index)}
-              bonus={
-                cardBonuses[index]
-              } /* Pass the bonus for the card */
+              imgId={card.id}
+              isFlipped={card.isFlip}
+              onCardClick={() => {
+                if (!card.isFlip) {
+                  card.isFlip = true;
+                  setCheckCards([...checkCards, card]);
+                } else {
+                  return;
+                }
+              }}
             />
           </div>
         ))}
